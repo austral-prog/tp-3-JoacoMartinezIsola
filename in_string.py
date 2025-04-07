e70a7544b1f72be0e7061babb8680cdc0171e584
JoacoMartinezIsola <jmartinezisola@mail.austral.edu.ar>
2025-04-07 17:41:49 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,13 @@
 def check_vowels():
+    name = input("Escribe su nombre:")
+    print("Contiene a:",  "a" in name.lower())
+    print("Contiene e:", "e" in name.lower())
+    print("Contiene i:", "i" in name.lower())
+    print("Contiene o:", "o" in name.lower())
+    print("Contiene u:", "u" in name.lower())
+    
+
+check_vowels()
     # Código a implementar utilizando input.
 
 
