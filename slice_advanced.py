145840c411abc806a906d35310ec6a4b1c689aca
JoacoMartinezIsola <jmartinezisola@mail.austral.edu.ar>
2025-04-08 14:16:13 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,8 +1,7 @@
 def slice_advanced():
-Name = input("")
+    Name = input("")
     print(Name[4: :2])
-
-    # Código a implementar utilizando input.
+   # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
